00a2a6310d3be326dad6aa1c100e32590c248c39
lvwenlong <lvwenlong_lambda@qq.com>
2014-08-08 18:17:52 +0800

    adding comment to test github

--- check_time.scm
+++ check_time.scm
@@ -1,6 +1,7 @@
 ;;检测一个函数运行的时间.
 ;;Author 吕文龙
 ;;Date: 2013/8/28
+;;test github
 (define (check-time proc . parameters)
   (define (internal-proc proc parameters start_time)
 	(apply proc parameters)
