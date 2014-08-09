afc5f7a070631646cc2cc39ad109607d905cea14
lvwenlong <lvwenlong_lambda@qq.com>
2014-08-09 21:30:04 +0800

    try to implement some new grammar using macro
    Signed-off-by: lvwenlong <lvwenlong_lambda@qq.com>

---
+++ R5RS/if_sugar.scm
@@ -0,0 +1,30 @@
+;;hygiene macro
+;;example of r5rs
+(define x 3)
+(define-syntax m
+  (syntax-rules ()
+                ((m) x)))
+(define (test-hygiene-macro)
+  (let ((x 4))
+    (m)));;3
+;;-------------------------------------------------------------------------
+(define-syntax iff
+  (syntax-rules (then els endif)
+                ((iff a then b endif)
+                 (if a b))
+                ((iff a then b els c endif)
+                 (if a b c))))
+(define-syntax -->
+  (syntax-rules (iif)
+                ((fuck a iif b)
+                 (if b a))))
+
+(--> 3 iif (> 1 0));;3  实现类似perl的die if ($fucked)的语法
+(define (fuck)
+  (let ((a 1))
+    (iff (> a 0)
+         then 
+            (display "positive")
+         els  
+            (display "negative")
+         endif)))
